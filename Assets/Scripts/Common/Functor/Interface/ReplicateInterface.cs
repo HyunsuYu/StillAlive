0aad7c96d5eab9fc389fb9a3d641e8a8dd804973
HyunsuYu <lioie6478@gmail.com>
2025-09-07 20:23:07 +0900

    Update

--- Assets/Scripts/Common/Functor/Interface/ReplicateInterface.cs
+++ Assets/Scripts/Common/Functor/Interface/ReplicateInterface.cs
@@ -45,11 +45,9 @@ public sealed class ReplicateInterface : SingleTonForGameObject<ReplicateInterfa
     }
     public void FixedUpdate()
     {
-        if (m_bisRequestProcessing && m_bisResponsed)
+        if(m_bisRequestProcessing && m_bisResponsed)
         {
             m_responseArrived.Invoke();
-            m_bisRequestProcessing = false;
-            m_bisResponsed = false;
         }
     }
 
@@ -101,38 +99,38 @@ public sealed class ReplicateInterface : SingleTonForGameObject<ReplicateInterfa
 
             case CardData.LastNightState.AttackedPerson:
                 prompt += $"당신은 지난 밤에 공격받은 인간으로서, 피의자가 ";
-                switch (selectedPart)
+                switch(selectedPart)
                 {
                     case CardData.NPCLookPartType.Top:
-                        prompt += $"{m_npcLookPark.Tops[attacher.Value.NPCLookTable[CardData.NPCLookPartType.Top]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.Tops[targetCardData.NPCLookTable[CardData.NPCLookPartType.Top]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
 
                     case CardData.NPCLookPartType.Face:
-                        prompt += $"{m_npcLookPark.Faces[attacher.Value.NPCLookTable[CardData.NPCLookPartType.Face]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.Faces[targetCardData.NPCLookTable[CardData.NPCLookPartType.Face]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
 
                     case CardData.NPCLookPartType.Eye:
-                        prompt += $"{m_npcLookPark.Eyes[attacher.Value.NPCLookTable[CardData.NPCLookPartType.Eye]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.Eyes[targetCardData.NPCLookTable[CardData.NPCLookPartType.Eye]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
 
                     case CardData.NPCLookPartType.Mouth:
-                        prompt += $"{m_npcLookPark.Mouths[attacher.Value.NPCLookTable[CardData.NPCLookPartType.Mouth]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.Mouths[targetCardData.NPCLookTable[CardData.NPCLookPartType.Mouth]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
 
                     case CardData.NPCLookPartType.Glasses:
-                        prompt += $"{m_npcLookPark.Glasses[attacher.Value.NPCLookTable[CardData.NPCLookPartType.Glasses]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.Glasses[targetCardData.NPCLookTable[CardData.NPCLookPartType.Glasses]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
 
                     case CardData.NPCLookPartType.Cap:
-                        prompt += $"{m_npcLookPark.Caps[attacher.Value.NPCLookTable[CardData.NPCLookPartType.Cap]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.Caps[targetCardData.NPCLookTable[CardData.NPCLookPartType.Cap]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
 
                     case CardData.NPCLookPartType.FrontHair:
-                        prompt += $"{m_npcLookPark.FrontHairs[attacher.Value.NPCLookTable[CardData.NPCLookPartType.FrontHair]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.FrontHairs[targetCardData.NPCLookTable[CardData.NPCLookPartType.FrontHair]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
 
                     case CardData.NPCLookPartType.BackHair:
-                        prompt += $"{m_npcLookPark.BackHairs[attacher.Value.NPCLookTable[CardData.NPCLookPartType.BackHair]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.BackHairs[targetCardData.NPCLookTable[CardData.NPCLookPartType.BackHair]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
                 }
                 break;
@@ -142,35 +140,35 @@ public sealed class ReplicateInterface : SingleTonForGameObject<ReplicateInterfa
                 switch (selectedPart)
                 {
                     case CardData.NPCLookPartType.Top:
-                        prompt += $"{m_npcLookPark.Tops[attacher.Value.NPCLookTable[CardData.NPCLookPartType.Top]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.Tops[targetCardData.NPCLookTable[CardData.NPCLookPartType.Top]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
 
                     case CardData.NPCLookPartType.Face:
-                        prompt += $"{m_npcLookPark.Faces[attacher.Value.NPCLookTable[CardData.NPCLookPartType.Face]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.Faces[targetCardData.NPCLookTable[CardData.NPCLookPartType.Face]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
 
                     case CardData.NPCLookPartType.Eye:
-                        prompt += $"{m_npcLookPark.Eyes[attacher.Value.NPCLookTable[CardData.NPCLookPartType.Eye]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.Eyes[targetCardData.NPCLookTable[CardData.NPCLookPartType.Eye]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
 
                     case CardData.NPCLookPartType.Mouth:
-                        prompt += $"{m_npcLookPark.Mouths[attacher.Value.NPCLookTable[CardData.NPCLookPartType.Mouth]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.Mouths[targetCardData.NPCLookTable[CardData.NPCLookPartType.Mouth]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
 
                     case CardData.NPCLookPartType.Glasses:
-                        prompt += $"{m_npcLookPark.Glasses[attacher.Value.NPCLookTable[CardData.NPCLookPartType.Glasses]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.Glasses[targetCardData.NPCLookTable[CardData.NPCLookPartType.Glasses]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
 
                     case CardData.NPCLookPartType.Cap:
-                        prompt += $"{m_npcLookPark.Caps[attacher.Value.NPCLookTable[CardData.NPCLookPartType.Cap]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.Caps[targetCardData.NPCLookTable[CardData.NPCLookPartType.Cap]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
 
                     case CardData.NPCLookPartType.FrontHair:
-                        prompt += $"{m_npcLookPark.FrontHairs[attacher.Value.NPCLookTable[CardData.NPCLookPartType.FrontHair]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.FrontHairs[targetCardData.NPCLookTable[CardData.NPCLookPartType.FrontHair]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
 
                     case CardData.NPCLookPartType.BackHair:
-                        prompt += $"{m_npcLookPark.BackHairs[attacher.Value.NPCLookTable[CardData.NPCLookPartType.BackHair]].Description}을(를) 가지고 있는 것을 보았다";
+                        prompt += $"{m_npcLookPark.BackHairs[targetCardData.NPCLookTable[CardData.NPCLookPartType.BackHair]].Description}을(를) 가지고 있는 것을 보았다";
                         break;
                 }
                 break;
